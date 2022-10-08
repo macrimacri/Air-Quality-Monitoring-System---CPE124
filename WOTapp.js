42b0bdbec52b976c4db1bcb2d4c9fd2e37e42505
Marc Lawrence Pua <83485910+PUAMarcLawrence@users.noreply.github.com>
2022-10-08 12:02:30 +0800

    Add files via upload

--- WOTapp.js
+++ WOTapp.js
@@ -1,20 +1,59 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000; //PORT
 app.use(express.json());
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
-app.use(express.static(path.join(__dirname,'images')));
+app.use(express.static(path.join(__dirname,'public')));
+
+/*
+const sensorVals=[
+    {id:1, name:'Humidity', reading:'0'},
+    {id:2, name:'Temperature', reading:'0'},
+    {id:3, name:'Carbon Monoxide Reading', reading:'0'},
+    {id:4, name:'Carbon Dioxide Reading', reading:'0'},
+];
+*/
+
+const members =[
+    {name:'Marc Lawrence C. Pua'},
+    {name:'Macrisanta Jasmin'},
+    {name:'Jasper Adrian S. Escala'},
+    {name:'Rodmar Lanuza'},
+];
 
 //Home
 app.get('/', (req,res) => {
     res.render('index',{
-        title: 'Air-Quality Monitoring System'
+        title: 'Home'
+    });
+});
+
+//Dashboard
+app.get('/dashboard', (req,res) => {
+    res.render('dashboard',{
+        title: 'Dashboard'
+    });
+});
+
+//about
+app.get('/about', (req,res) => {
+    res.render('about',{
+        title: 'About'
+    });
+});
+
+//team
+app.get('/team', (req,res) => {
+    res.render('team',{
+        title: 'Team',
+        team: members 
     });
 });
 
 
 
-const port = process.env.PORT || 3000; //PORT
+
 app.listen(port, () => console.log(`Server Started...Port ${port}`));
