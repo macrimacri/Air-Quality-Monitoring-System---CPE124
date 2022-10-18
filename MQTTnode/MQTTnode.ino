22d2836764324018af90a83bdadcbe8ea70b77d3
Marc Lawrence Pua <83485910+PUAMarcLawrence@users.noreply.github.com>
2022-10-19 03:45:56 +0800

    Add files via upload

--- MQTTnode/MQTTnode.ino
+++ MQTTnode/MQTTnode.ino
@@ -5,23 +5,23 @@
 #include <MQ135.h>
 #define SERVER_IP "raspberrypi.local:3000"
 #ifndef STASSID
-#define STASSID "**********"
-#define STAPSK "*************"
+#define STASSID "PuaExtention 2.4"
+#define STAPSK "Pua@SurePass"
 #endif
 
+#define SIG A0
+#define S0 5
+
 DHT dht (2,DHT11);
-MQ135 mq135S(A0);
+MQ135 mq135S(SIG);
 String dataN;  
 void setup() {
 
   Serial.begin(115200);
-
   Serial.println();
   Serial.println();
   Serial.println();
-
   WiFi.begin(STASSID, STAPSK);
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -30,6 +30,8 @@ void setup() {
   Serial.print("Connected! IP address: ");
   Serial.println(WiFi.localIP());
   dht.begin();
+  pinMode(S0,OUTPUT);
+  pinMode(SIG,INPUT);
 }
 
 void loop() {
@@ -37,13 +39,14 @@ void loop() {
   if ((WiFi.status() == WL_CONNECTED)) {
     WiFiClient client;
     HTTPClient http;
-    String temp = String(dht.readTemperature(),2);
-    String Rhum = String(dht.readHumidity());
-    float rzero = mq135S.getRZero();
-    Serial.println(rzero);
-    float correctedPPM = mq135S.getCorrectedPPM(dht.readTemperature(),dht.readHumidity());
-    Serial.println(correctedPPM);
+    String temp = String(dht.readTemperature(),1);
+    String Rhum = String(dht.readHumidity(),1);
+    digitalWrite(S0,LOW);
+    float correctedPPM = analogRead(SIG);//mq135S.getCorrectedPPM(dht.readTemperature(),dht.readHumidity());
     String co2 = String(correctedPPM,2);
+    digitalWrite(S0,HIGH);
+    float mq7 = analogRead(SIG);
+    String co = String(mq7,2);
     Serial.print("[HTTP] begin...\n");
     // configure traged server and url
     http.begin(client, "http://" SERVER_IP "/sensorUpdate");  // HTTP
@@ -53,6 +56,7 @@ void loop() {
     doc["temp"] = temp;
     doc["Rhum"] = Rhum;
     doc["co2"] = co2;
+    doc["co"] = co;
     serializeJson(doc,dataN);
     // start connection and send HTTP header and body
     int httpCode = http.PUT(dataN);
